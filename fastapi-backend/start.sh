91b0b90d3a91d01c1b2eedef3b5ef88d4bb332c8
Ajesh Mishra <ajesh.sit@gmail.com>
2026-03-15 13:19:06 +0000

    Windows specifc devcontainer changes and adds gitattributes file

--- fastapi-backend/start.sh
+++ fastapi-backend/start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Running Database Migrations"
 uv run alembic upgrade head
+
+echo "Starting FastAPI Development Server"
 uv run fastapi dev app/main.py --host 0.0.0.0 --port 8000
+
+# echo "Starting FastAPI Production Server"
 # uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
